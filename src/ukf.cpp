324fd7456b9de66d3763f5085ccba0eb6e68cea2
justinbellucci <justin.bellucci@me.com>
2021-10-02 12:25:03 -0600

    add augmented sigma point matrices

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -21,12 +21,15 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = Eigen::MatrixXd(5, 5);
 
+  // TODO: Set the noise values
   // Process noise standard deviation longitudinal acceleration in m/s^2
   std_a_ = 30; // rule of thumb divided expected max acceleration by 2
 
   // Process noise standard deviation yaw acceleration in rad/s^2
   std_yawdd_ = 30;
   
+  ////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
   // Provided by sensor manufacturer
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -43,11 +46,12 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
   // end provided by sensor manufacturer
+  ////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
 
   n_x_ = x_.size(); // get size of state vector
   n_aug_ = n_x_ + 2; // get size of augmented state vector
   lambda_ = 3 - n_aug_; // define spreading parameter
-
   // initialize params for predicting mean and covariance with augmented state vector
   weights_ = Eigen::VectorXd(2 * n_aug_ + 1); // define weights vector size
   weights_(0) = lambda_ / (lambda_ + n_aug_); 
@@ -107,7 +111,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     time_us_ = meas_package.timestamp_; //  measure time
     return; 
 
-  } else { // not initialized
+  } else { // initialized
     // compute delta time
     double dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
     // predict state
@@ -128,6 +132,32 @@ void UKF::Prediction(double delta_t) {
    * Modify the state vector, x_. Predict sigma points, the state, 
    * and the state covariance matrix.
    */
+  // create augmented matrices
+  Eigen::VectorXd x_aug = Eigen::VectorXd(n_aug_); // augmented state mean vector
+  Eigen::MatrixXd P_aug = Eigen::MatrixXd(n_aug_, n_aug_); // augmented state covariance matrix
+  Eigen::MatrixXd Xsig_aug = Eigen::MatrixXd(n_aug_, 2 * n_aug_ + 1); // augmented sigma points matrix
+
+  // create augmented mean state vector
+  x_aug.head(5) = x_; // first 5 elements fill with x_
+  x_aug(5) = 0; // initial value for longitudinal acceleration noise
+  x_aug(6) = 0; // initial value for yaw acceleration noise
+
+  // create augmented covariance matrix
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(n_x_, n_x_) = P_; // fill top left with P_
+  P_aug(5, 5) = std_a_ * std_a_; // longitudinal acceleration noise
+  P_aug(6, 6) = std_yawdd_ * std_yawdd_; // yaw angle acceleration noise
+
+  // create augmented sigma points matrix
+  Eigen::MatrixXd sqrt_P_aug = P_aug.llt().matrixL(); // square root of P_aug
+  double sqrt_lambda = sqrt(lambda_ + n_aug_); 
+  Xsig_aug.col(0) = x_aug; // first column is the mean state
+  for (int i = 0; i < n_aug_; i++) {
+    Xsig_aug.col(i + 1) = x_aug + sqrt_lambda * sqrt_P_aug.col(i); 
+    Xsig_aug.col(i + 1 + n_aug_) = x_aug - sqrt_lambda * sqrt_P_aug.col(i);
+  }
+  
+  // predict sigma points
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
